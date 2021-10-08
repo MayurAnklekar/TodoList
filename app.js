ff3812e90343cca647d0bc638d0b4cf8d3f065dc
MayurAnklekar <mayuranklekar1010@gmail.com>
2021-10-08 18:37:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const ejs = require("ejs");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("login");
+    
+})
+
+app.listen(3000, function(req, res){
+    console.log("Server running");
+})
