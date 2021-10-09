89cdde8a3a6662ab4b43b1b586cf62b3c455fbe2
MayurAnklekar <mayuranklekar1010@gmail.com>
2021-10-09 12:20:42 +0530

    Linked pages and added signin options in login page

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require("express");
-
 const ejs = require("ejs");
+
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
-    res.render("list");
-    
-})
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
-app.listen(3000, function(req, res){
-    console.log("Server running");
-})
+app.get("/list", function (req, res) {
+  res.render("list");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.listen(3000, function (req, res) {
+  console.log("Server running");
+});
